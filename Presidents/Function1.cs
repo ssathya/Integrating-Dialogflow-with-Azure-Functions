cdcec75f2c477aa1a633ec9aa10d1cdd124431d8
Sridhar Sathya <sridhar.sathya@live.com>
2019-04-04 13:41:08 -0400

    Reorganize the code.

--- Presidents/Function1.cs
+++ Presidents/Function1.cs
@@ -1,34 +1,39 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Google.Cloud.Dialogflow.V2;
+using Google.Protobuf;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Google.Cloud.Dialogflow.V2;
-using Google.Protobuf;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Presidents
 {
-    public static class Function1
-    {
+	public static class Function1
+	{
+
+		#region Private Fields
+
 		private static readonly JsonParser jsonParser =
 		new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
 
+		#endregion Private Fields
+
+
+		#region Public Methods
+
 		[FunctionName("President")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("Starting President service.");
+		public static async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+			ILogger log)
+		{
+			log.LogInformation("Starting President service.");
 
 			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 			WebhookRequest request;
 			request = jsonParser.Parse<WebhookRequest>(requestBody);
 			var firstParamName = request.QueryResult.Parameters.Fields["presidentName"].ToString().Replace("\"", "");
-
 
 			var response = new WebhookResponse
 			{
@@ -41,7 +46,8 @@ namespace Presidents
 				ContentType = "application/json",
 				StatusCode = 200
 			};
+		}
 
-        }
-    }
+		#endregion Public Methods
+	}
 }
